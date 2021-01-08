cd5833b8b8be78c1a70b579bc35a49864d83d94a
Eric Ren <eric@renf.ca>
2021-01-07 20:04:18 -0800

    clean up

--- src/pages/nav.comp.js
+++ src/pages/nav.comp.js
@@ -55,13 +55,11 @@ const getTopshotOverview = async () => {
 export function TopShotNav() {
   const [error, setError] = useState(null)
   const [accountAddress, setAccountAddress] = useState("")
-  const [topshotOverview, setTopshotOverview] = useState(null)
   const [seriesSets, setSeriesSets] = useState(null)
   useEffect(() => {
     getTopshotOverview()
       .then((d) => {
         console.log(d)
-        setTopshotOverview(d)
         setSeriesSets(splitSets(d.sets))
       })
       .catch(setError)
@@ -75,7 +73,7 @@ export function TopShotNav() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="/plays">Plays</Nav.Link>
-          {seriesSets && Object.entries(splitSets(topshotOverview.sets)).map(
+          {seriesSets && Object.entries(seriesSets).map(
             ([series, sets],i) => {
               return (
                 <NavDropdown key={series} title={`s${series} sets`} id="basic-nav-dropdown">
